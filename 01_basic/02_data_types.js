49cef4791dee64c065a73169f36acd90206a38ac
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-24 19:37:21 +0000

    data types in making

---
+++ 01_basic/02_data_types.js
@@ -0,0 +1,33 @@
+let age = 24
+let name = "Tushar Singh"
+let school = "Baal Baari Public School"
+let thristy = true
+// console.table([name, age, school, thristy])
+
+
+
+
+/* Datatypes
+number -> 2 to power 53
+bigint
+string = ""
+boolean => true/false
+
+null -> standalone value hai, abhi assign nahi huyi hai, represention hai empty value ka
+undefined -> not assigning any value
+
+symbol -> unique identify krne ke liye REACT mein..
+
+
+
+object 
+*/
+console.log(typeof "Hello")
+console.log(typeof age)
+console.log(typeof null)
+
+console.log(typeof thristy)
+console.log(typeof undefined)
+
+
+console.log(typeof "Tsuahr")
