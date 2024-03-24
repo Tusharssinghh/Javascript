8f1bd8abcace3d56f16ccfeeddc3e2fbc600ceee
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-24 19:35:17 +0000

    variable in making

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+// JavaScript is an Interpreted language and it is a compiled programming language
+
+const name = "Tushar singh"
+let number = 8439789923
+var school = "Baal Baari Public School"
+console.log(name)
+
+//Try not to use var because of use of block scope and functional scope 
+
+//printing multiple variables 
+console.log([name, number, school])
+console.table([name, number, school])
+
