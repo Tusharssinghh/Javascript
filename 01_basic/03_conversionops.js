afda7f6f10e17bc76cff1ea417b7cd09ef47f336
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-25 17:24:04 +0000

    conversion hits

---
+++ 01_basic/03_conversionops.js
@@ -0,0 +1,58 @@
+//To convert write first letter in capital letter
+
+
+let score = "33";
+// console.log(typeof(score))
+
+let ans = Number(score)
+// console.log(typeof(ans))
+// console.log(ans)
+
+
+
+let name = "Tushar"
+let ans1 = Number(name)
+// console.log(ans1)
+
+let bool = null
+let ans2 = Number(bool)
+// console.log(ans2)
+
+// 0 -> false;
+// 1 -> true;
+// "" -> false;
+// "tushar" -> true;
+
+/* ************************************OPERATIONS***********************************************************/
+
+let num = 33
+let num_to_negative = -num
+// console.log(num_to_negative);
+
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+// console.log(2**3)
+
+// let str1 = "Tushar "
+// let str2 = "Singh"
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// //IMPORTANT
+// console.log("1" + 1)
+// console.log(1 + "1")
+// console.log("1" + 1 + 1)
+// console.log(1 + 1 + "1")
+
+// console.log(+true)
+// console.log(-false)
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 4;
+// console.log(num1 + num2 + num3)
+
+
