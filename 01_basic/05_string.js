df9f11c3b046e0220e6fc64e992cbfbb4172817c
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-26 18:17:26 +0000

    Learning string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,51 @@
+// const name = "Tushar "
+// const name1 = " Singh"
+// console.log(name + 100 + name1)
+
+
+//Instead of string concatentaion, we use string interpolation..
+// ${} -> is called string placeholder..
+// let namee = "Tushar Singh"
+// console.log(`My name is ${namee}`)
+
+// const name = "Tushar singh"
+// const repocount = 50
+// console.log(`My name is ${name} and my count is ${repocount}`)
+
+
+
+// let str = "Tushar"
+// let str1 = "Singh"
+// console.log(`The New string is ${str} and the new is ${str1}`)
+
+// console.log(str[0])
+
+// const strin = new String('Tushar-Singh-desi')
+// console.log(strin[4])
+
+// console.log(strin.__proto__)
+// console.log(strin.length)
+
+// console.log(strin.toUpperCase())
+
+// console.log(strin.charAt(2))
+
+// console.log(strin.indexOf('s'))
+
+// const newstring = strin.substring(0,6)
+// console.log(newstring)
+
+// const newstring1 = strin.slice(-5, -1)
+// console.log(newstring1)
+
+// const string1 = "   Tushar   "
+// console.log(string1.trim())
+// console.log(string1.trimEnd())
+// console.log(string1.trimStart())
+
+// console.log(string1.includes('Tush'))
+
+// const strnew = strin.split('-')
+// console.log(strnew)
+
+
