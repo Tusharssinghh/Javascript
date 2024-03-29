729f45af24e85c9b2abc245aae22b47c7c6d13d5
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-29 17:54:09 +0000

    If, else, switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// In Javascript everytime global execution and function execution context is made..
+
+
+
+
+//     == check for equality
+//     === check for datatype and equality
+//     && ||   -> logical operator
+
+//     let str = ""
+//     if(str)....this is consider as empty string....
+//     let str1 = []
+//     if(str1)....this is not consider as empty string....
+
+// const userloggedin = true
+// if(2 === "2")
+//     console.log("User is here, Please don't do stupid things..")
+// else if(!userloggedin){
+//     console.log("user isn't here, you can do whatever you want")
+// }
+// else{
+//     console.log("please go away, i'm not feeling good")
+// }
+
+// const userid = 49 
+// if(userid === 49) console.log("The id of user is 49"), console.log("hello");
