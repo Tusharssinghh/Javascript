67c8b6dd35f629f446af883d7922b2b57c9a04e9
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-29 19:31:59 +0000

    For loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,22 @@
+//for loop
+// for (let i = 0; i<= 10 ; i++){
+//     console.log(i)
+// }
+//printing table...
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//        console.log(i + ' * ' + j + '=' + i*j  )
+//     }
+// }
+
+// let arr = ["one", "two", "three"]
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+for(let i=1; i<=20; i++){
+    if(i == 5){
+        console.log(`5 detected`)
+        break
+    }
+    console.log(`The value of i is ${i}`)
+}
