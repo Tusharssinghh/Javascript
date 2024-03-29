92dfb4dfc256b7cee26d221fd4b7013bacf0f4d0
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-29 18:22:36 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let userid;
+switch(userid){
+    case 1:
+        console.log("Enter 1");
+        break;
+    case 2: 
+        console.log("Enter 2");
+        break;
+    case 3:
+        console.log("Enter 3");
+        break;
+    case 4:
+        console.log("Enter 4");
+        break;
+    default:
+        console.log("Nothing match")
+}
