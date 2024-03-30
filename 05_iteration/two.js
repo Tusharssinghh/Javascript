ff9efa02ee88f1ec585181e436584227d6b27f2e
Tusharssinghh <130763713+Tusharssinghh@users.noreply.github.com>
2024-03-30 05:46:44 +0000

    Do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+// let index = 1
+// while(index <= 10){
+//     console.log(`The value of i is ${index}`)
+//     index = index + 2
+// }
+// let arr = ['Tushar', 'singh', 'Aakansha', 'singh']
+// let i = 0
+// while(i < arr.length){
+//     console.log(`The names are ${arr[i]}`)
+//     i++
+// }
+// let i = 1
+// do{
+//     console.log(`The value of i is ${i}`)
+//     i++
+// }while(i <= 10)
+
+let i = 11
+do{
+    console.log(`The value of i is ${i}`)
+    i++
+}while(i <= 10)
